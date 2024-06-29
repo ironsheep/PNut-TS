ee0ba29dbc7e1c84f445b00e98b9169c7fb977b2
Stephen M Moraco <stephen@ironsheep.biz>
2024-06-28 18:20:51 -0600

    update tests with real pnut_v32 files - add more run* targets
    - clean up jest test-config files
    - update some individual run files so we can find code parsing issues

---
+++ src/tests/LOADER-tests/pnut-ts-loader.test.ts
@@ -0,0 +1,104 @@
+/* eslint-disable no-console */
+'use strict';
+import { execSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+// Import the glob function specifically
+//import { glob } from 'glob';
+
+// Alternatively, if you want to use the synchronous version, you can do:
+import { sync as globSync } from 'glob';
+import { compareListingFiles, compareObjOrBinFiles, removeExistingFile, topLevel } from '../testUtils';
+
+// test lives in <rootDir>/src/tests/FULL
+const dirPath = path.resolve(__dirname, '../../../TEST/LOADER-tests');
+const toolPath = path.resolve(__dirname, '../../../dist');
+
+const directories = [
+  { name: 'Test directory', path: dirPath, relFolder: dirPath.replace(topLevel, './') },
+  { name: 'Tool directory', path: toolPath, relFolder: toolPath.replace(topLevel, './') }
+];
+
+describe('Directory existence tests', () => {
+  test.each(directories)('$relFolder should exist', ({ path }) => {
+    if (!fs.existsSync(path)) {
+      throw new Error(`Directory does not exist: ${path}`);
+    }
+  });
+});
+
+describe('PNut_ts builds our LOADER test files correctly', () => {
+  let files: string[] = [];
+  try {
+    files = globSync(`${dirPath}/*.spin2`);
+  } catch (error) {
+    console.error('ERROR: glob issue:', error);
+  }
+  if (files.length > 1) {
+    files.sort();
+  }
+  files.forEach((file) => {
+    test(`Compile file: ${path.basename(file)}`, () => {
+      const options: string = '-v -l -d -O --regression element --';
+      const basename = path.basename(file, '.spin2');
+
+      const listingFSpec = path.join(dirPath, `${basename}.lst`);
+      const objectFSpec = path.join(dirPath, `${basename}.obj`);
+      const binaryFSpec = path.join(dirPath, `${basename}.bin`);
+      const elementsFSpec = path.join(dirPath, `${basename}.elem`);
+
+      // Remove existing files
+      removeExistingFile(listingFSpec);
+      removeExistingFile(objectFSpec);
+      removeExistingFile(binaryFSpec);
+      removeExistingFile(elementsFSpec);
+
+      // compile our file generating output files
+      try {
+        execSync(`node ${toolPath}/pnut-ts.js ${options} ${file}`);
+      } catch (error) {
+        console.error(`ERROR: running PNut-TS: ${error}`);
+        fail(`Execution failed for ${file}`);
+      }
+
+      // count the number of matching outputs, should be 3!
+      let whatFailed: string = '';
+      // ID the golden listing file
+      const goldenFSpec = path.join(dirPath, `${basename}.lst.GOLD`);
+      // Compare listing files
+      let filesMatch: boolean = compareListingFiles(listingFSpec, goldenFSpec);
+      if (!filesMatch) {
+        whatFailed = appendDiagnosticString(whatFailed, 'Listing Files', ', ');
+      }
+
+      // ID the golden .obj file
+      const goldenObjFSpec = path.join(dirPath, `${basename}.obj.GOLD`);
+      // Compare object files
+      filesMatch = compareObjOrBinFiles(objectFSpec, goldenObjFSpec);
+      if (!filesMatch) {
+        whatFailed = appendDiagnosticString(whatFailed, 'Object Files', ', ');
+      }
+
+      // ID the golden .bin file
+      const goldenBinFSpec = path.join(dirPath, `${basename}.bin.GOLD`);
+      // Compare binary files
+      filesMatch = compareObjOrBinFiles(binaryFSpec, goldenBinFSpec);
+      if (!filesMatch) {
+        whatFailed = appendDiagnosticString(whatFailed, 'Binary Files', ', ');
+      }
+
+      if (whatFailed.length > 0) {
+        whatFailed = appendDiagnosticString(whatFailed, "Don't match!", ' ');
+      }
+      expect(whatFailed).toBe('');
+    });
+  });
+});
+
+function appendDiagnosticString(origString: string, appendString: string, separator: string): string {
+  let longerString: string = appendString;
+  if (origString.length > 0) {
+    longerString = `${origString}${separator}${appendString}`;
+  }
+  return longerString;
+}
